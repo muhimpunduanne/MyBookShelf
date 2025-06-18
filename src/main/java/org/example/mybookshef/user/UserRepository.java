9918ebe4e1951f72c36b07e9486bfee813ccf464
Gloria <mutuyegloria@gmail.com>
2025-06-17 23:30:48 -0700

    add all changes

--- src/main/java/org/example/mybookshef/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.mybookshef.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  UserRepository extends JpaRepository<Users, Long> {
-    Boolean existsByEmail(String email);
-   Boolean existsByRegNo (String regNo);
-}
